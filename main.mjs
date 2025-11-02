f55cf0b4a9def96fa3b056a1581702bf3ab89a7e
watayoshi1984 <w.yoshiki1984@gmail.com>
2025-11-02 15:54:28 +0900

    Fix: ログインボタンのセレクタを修正

---
+++ main.mjs
@@ -0,0 +1,48 @@
+import puppeteer from 'puppeteer'
+import { setTimeout } from 'node:timers/promises'
+
+const args = ['--no-sandbox', '--disable-setuid-sandbox']
+if (process.env.PROXY_SERVER) {
+    const proxy_url = new URL(process.env.PROXY_SERVER)
+    proxy_url.username = ''
+    proxy_url.password = ''
+    args.push(`--proxy-server=${proxy_url}`.replace(/\/$/, ''))
+}
+
+const browser = await puppeteer.launch({
+    defaultViewport: { width: 1080, height: 1024 },
+    args,
+})
+const [page] = await browser.pages()
+const userAgent = await browser.userAgent()
+await page.setUserAgent(userAgent.replace('Headless', ''))
+const recorder = await page.screencast({ path: 'recording.webm' })
+
+try {
+    if (process.env.PROXY_SERVER) {
+        const { username, password } = new URL(process.env.PROXY_SERVER)
+        if (username && password) {
+            await page.authenticate({ username, password })
+        }
+    }
+
+    await page.goto('https://secure.xserver.ne.jp/xapanel/login/xvps/', { waitUntil: 'networkidle2' })
+    await page.locator('#memberid').fill(process.env.EMAIL)
+    await page.locator('#user_password').fill(process.env.PASSWORD)
+    await page.locator('//*[@id="login_area"]/div[2]/div[1]/input[2]').click()
+    await page.waitForNavigation({ waitUntil: 'networkidle2' })
+    await page.locator('a[href^="/xapanel/xvps/server/detail?id="]').click()
+    await page.locator('text=更新する').click()
+    await page.locator('text=引き続き無料VPSの利用を継続する').click()
+    await page.waitForNavigation({ waitUntil: 'networkidle2' })
+    const body = await page.$eval('img[src^="data:"]', img => img.src)
+    const code = await fetch('https://captcha-120546510085.asia-northeast1.run.app', { method: 'POST', body }).then(r => r.text())
+    await page.locator('[placeholder="上の画像の数字を入力"]').fill(code)
+    await page.locator('text=無料VPSの利用を継続する').click()
+} catch (e) {
+    console.error(e)
+} finally {
+    await setTimeout(5000)
+    await recorder.stop()
+    await browser.close()
+}
